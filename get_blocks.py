297be1c463bb2973131b663dab10c2754fb3d004
davidgonchar <david.gonchar@gmail.com>
2016-06-13 12:47:00 +0300

    fixed: 60 sec delay

--- get_blocks.py
+++ get_blocks.py
@@ -7,13 +7,11 @@ import blocktrail
 client = blocktrail.APIClient(api_key="731ac7a92afe05e7640b3e89d69ecce514d3ea1d", api_secret="cb816f06d9722244f7489deb71cc65c43b97bccd", network="BTC", testnet=False)
 
 address = client.address('3ETGrUjxeuC1e8nY8EP7U2EUcvffX76AKC')
-latest_block = client.block_latest()
 
-print(latest_block)
-
-prev_block = 0
+prev_block = 416075
 
 while True:
+    latest_block = client.block_latest()
     last_block = latest_block['height']
     for i in xrange(prev_block, last_block):
         try:
@@ -26,7 +24,6 @@ while True:
 
         except Exception as e:
             print(e)
-
+    sleep(60)
     prev_block = last_block
-    sleep(1000)
-
+    print("==== %s ====" % str(prev_block))
